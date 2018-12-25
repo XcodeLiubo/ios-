3535fab3d92564e8698c2bf5e4a52c4d900cbbec
Liubo <liubo@163.com>
2018-12-25 11:22:54 +0800

    误删

---
+++ LBViewCornerRadius/LBViewCornerRadius/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  LBViewCornerRadius
+//
+//  Created by 刘泊 on 2018/12/25.
+//  Copyright © 2018 LB. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
